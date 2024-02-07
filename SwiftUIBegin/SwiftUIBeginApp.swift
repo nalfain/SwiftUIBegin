f58f71d23adeb7468cb192b0ff853ea6aee91037
Maxim Piratorov <piratorov@gmail.com>
2024-02-07 14:55:38 +0200

    Initial Commit

---
+++ SwiftUIBegin/SwiftUIBeginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBeginApp.swift
+//  SwiftUIBegin
+//
+//  Created by gost on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBeginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
