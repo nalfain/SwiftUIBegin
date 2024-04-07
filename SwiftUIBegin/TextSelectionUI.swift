295e46b8518e6e37633766920de60cf464d18e6e
Nalfain <Nalfain@icloud.com>
2024-04-07 12:41:48 +0300

    add Text SelectionUI

---
+++ SwiftUIBegin/TextSelectionUI.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionUI.swift
+//  SwiftUIBegin
+//
+//  Created by MP on 07.04.2024.
+//
+
+import SwiftUI
+
+struct TextSelectionUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionUI_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionUI()
+    }
+}
