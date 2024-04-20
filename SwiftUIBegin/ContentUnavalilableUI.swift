7e2bd73181f845588be8d7e29c6addfb2a00a8e5
Nalfain <Nalfain@icloud.com>
2024-04-20 15:26:21 +0300

    Add ContentUnavalilableUI

---
+++ SwiftUIBegin/ContentUnavalilableUI.swift
@@ -0,0 +1,27 @@
+//
+//  ContentUnavalilableUI.swift
+//  SwiftUIBegin
+//
+//  Created by MP on 20.04.2024.
+//
+
+import SwiftUI
+
+struct ContentUnavalilableUI: View {
+    var body: some View {
+// my xcode is not support
+//        ContentUnavailableView {
+//            Label("No Mail", systemImage: "tray.fill")
+//        } description: {
+//            Text("New mails you receive will appear here.")
+//        }
+        Text("Hi")
+   }
+}
+
+        
+struct ContentUnavalilableUI_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentUnavalilableUI()
+    }
+}
